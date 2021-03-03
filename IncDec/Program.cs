2c3d72a9b95ceca9f63e0feb53ed8e20d9891b06
Letícia Oliveira Torres <leticia@MacBook-Air-de-Leticia.local>
2021-03-03 10:02:07 +0000

    Adding IncDec

---
+++ IncDec/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace IncDec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 5;
+            int c = a++;
+            Console.WriteLine($"a = {a}");
+            Console.WriteLine($"b = {b}");
+            Console.WriteLine($"c = {c}");
+        }
+    }
+}
