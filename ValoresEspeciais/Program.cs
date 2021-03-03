c88ba017e1220829c973fa3bc38f491fffa971cc
Letícia Oliveira Torres <leticia@MacBook-Air-de-Leticia.local>
2021-03-03 12:30:10 +0000

    Adding project ValoresEspeciais

---
+++ ValoresEspeciais/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ValoresEspeciais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"int max = {int.MaxValue}");
+            Console.WriteLine($"int min = {int.MinValue}");
+            Console.WriteLine($"double max = {double.MaxValue}");
+            Console.WriteLine($"double min = {double.MaxValue}");
+            Console.WriteLine($"byte max = {byte.MaxValue}");
+            Console.WriteLine($"byte min = {byte.MaxValue}");
+
+            Console.WriteLine($"double +infinity = {double.PositiveInfinity}");
+            Console.WriteLine($"double -infinity = {double.NegativeInfinity}");
+            Console.WriteLine($"double NaN = {double.NaN}");
+
+        }
+    }
+}
