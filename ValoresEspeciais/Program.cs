3140412f6302a6d8fc57d6babc867ca9814df247
Letícia Oliveira Torres <leticia@MacBook-Air-de-Leticia.local>
2021-03-03 12:39:55 +0000

    Test for overflows

--- ValoresEspeciais/Program.cs
+++ ValoresEspeciais/Program.cs
@@ -17,6 +17,26 @@ namespace ValoresEspeciais
             Console.WriteLine($"double -infinity = {double.NegativeInfinity}");
             Console.WriteLine($"double NaN = {double.NaN}");
 
+            byte b = byte.MaxValue;
+            Console.WriteLine($"byte max = {b}");
+            b++;
+            Console.WriteLine($"byte max + 1= {b}");
+
+            short s = byte.MinValue;
+            Console.WriteLine($"short min = {s}");
+            s--;
+            Console.WriteLine($"short min - 1= {s}");
+
+            float f1;
+            f1 = 10000f;
+            f1 += 0.0001f;
+            Console.WriteLine($"f1 = {f1}");
+
+            double d1;
+            d1 = 10000f;
+            d1 += 0.0001f;
+            Console.WriteLine($"d1 = {d1}");
+
         }
     }
 }
